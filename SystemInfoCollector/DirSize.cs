d91e210bab296ae98a2ef9772da3a520e231729a
mar Jinn <marjinn@live.com>
2013-04-08 01:54:11 +0530

    added directory in use exception to recursive dir delete
    added directory in use exception to recursive dir delete

--- SystemInfoCollector/DirSize.cs
+++ SystemInfoCollector/DirSize.cs
@@ -181,6 +181,12 @@ namespace SystemInfoCollector
                     ///no rights to access the file
                     System.Diagnostics.Debug.WriteLine(subDirectory.FullName + "\n");
                 }
+
+                catch (System.IO.IOException)
+                {
+                    ///directory in use
+                    System.Diagnostics.Debug.WriteLine(subDirectory.FullName + "\n");
+                }
             }
         }
 
